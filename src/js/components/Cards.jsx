757dfd7ad3c9d8b10e9356276f79db23dfe51a6f
alejandrolobatogil-dot <alejandrolobatogil@gmail.com>
2025-12-01 10:23:40 +0000

    más dinamico

--- src/js/components/Cards.jsx
+++ src/js/components/Cards.jsx
@@ -1,10 +1,10 @@
-const Card = () => {
+const Card = ({img, title, description}) => {
     return (
         <div className="card">
-            <img src="https://concepto.de/wp-content/uploads/2020/08/Programacion-informatica-scaled-e1724960033513.jpg" className="card-img-top" alt="" />
+            <img src={img} className="card-img-top" alt="" />
             <div className="card-body">
-                <h5 className="card-title">Título</h5>
-                <p className="card-text">Contenido dentro de la card.</p>
+                <h5 className="card-title">{title}</h5>
+                <p className="card-text">{description}</p>
                 <a href="#" className="btn btn-primary">Acceso</a>
             </div>
         </div>
--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 import Card from "./Cards";
 import Navbar from "./Nav";
 import Titulo from "./Titulo";
@@ -15,10 +14,27 @@ const Home = () => {
 			<Navbar/>
             <Titulo/>
 			<div class="container d-flex justify-content-center">
-            <Card/>
-			<Card/>
-			<Card/>
-			<Card/>
+            <Card
+			title="Programacion"
+			description="Too fun"
+			img="https://msmk.university/wp-content/uploads/2025/02/Foto25.jpg"
+			/>
+
+			<Card
+			title="Programando"
+			description="In process"
+			img="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSuxteVNIzVwMwr6wrpQsAN9aMxpjERUBWNRA&s"
+			/>
+			<Card
+			title="JavaScript"
+			description="Learning"
+			img="https://upload.wikimedia.org/wikipedia/commons/a/a4/JavaScript_code.png"
+			/>
+			<Card
+			title="React"
+			description="Awesome"
+			img="https://miro.medium.com/v2/1*VLiW4zOLz6lZX9_TUzg0eQ.png"
+			/>
 			</div>
 			<Footer/>
 		</>
